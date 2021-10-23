a6473845dc042fdd9f33327b24fd31b8d515d14c
Hains <hainsvdbosch@ziggo.nl>
2021-10-23 16:37:57 +0200

    NetworkSetup.py: Decode data to unicode
    To fix network test.
    
       callback(result)
    <   File "/usr/lib/enigma2/python/Screens/NetworkSetup.py", line 1727, in LinkStatedataAvail
    <     if "Link detected:" in item:
    <   TypeError: a bytes-like object is required, not 'str'
    <  24116.0545> 16:29:54.4558 [ePyObject] (CallObject(<traceback object at 0xb2591808>,(0,)) failed)

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -1723,6 +1723,7 @@ class NetworkAdapterTest(Screen):
 			iNetwork.getLinkState(iface, self.LinkStatedataAvail)
 
 	def LinkStatedataAvail(self, data):
+		data = data.decode()
 		for item in data.splitlines():
 			if "Link detected:" in item:
 				if "yes" in item:
