3f5cc09a9af019d4f14eb641569ddefaa366b9cc
builder08 <75539906+builder08@users.noreply.github.com>
2021-11-12 01:33:31 +0200

    Update Wlan.py

--- lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
+++ lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
@@ -5,8 +5,6 @@ from Components.Network import iNetwork
 import enigma
 
 import os
-from pythonwifi.iwlibs import Wireless, getWNICnames
-from pythonwifi import flags as wififlags
 
 list = ["WPA/WPA2", "WPA2", "WPA", "WEP", "Unencrypted"]
 
